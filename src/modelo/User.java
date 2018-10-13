12facaca82bccba4b6edf4389f37b9bdfccdb03c
brunogeek9 <brunogeek9@gmail.com>
2018-10-13 19:23:28 -0300

    adicionando os arquivos

---
+++ src/modelo/User.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author jamelli
+ */
+public class User {
+    private int id, age;
+    private String first, last;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public User(int age, String first, String last) {
+        this.age = age;
+        this.first = first;
+        this.last = last;
+    }
+    public User(){}
+    
+}
