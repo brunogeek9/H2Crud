628811b5f1aad2d0f484527f976e6538d129c327
brunogeek9 <brunogeek9@gmail.com>
2018-10-13 19:32:58 -0300

    alterações nos nomes dos pacotes

--- src/principal/CriandoBanco.java
+++ src/principal/CriandoBanco.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testeh2;
+package principal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/principal/TesteH2.java
+++ src/principal/TesteH2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testeh2;
+package principal;
 
 import modelo.User;
 import persistencia.UserDAO;
